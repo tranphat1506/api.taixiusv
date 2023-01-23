cba45f77b119f852db2bda8686510bf64e5eb100
Lê Châu Trần Phát <lechautrphat@gmail.com>
2023-01-23 11:14:20 +0700

    initial commit 3

--- src/api/middlewares/auth.middleware.js
+++ src/api/middlewares/auth.middleware.js
@@ -50,14 +50,12 @@ async function verifyToken(req, res, next) {
                 .then(async arrayToken=>{
                     res.cookie('a_token', arrayToken[0].encoded, {
                         maxAge : 3600000, //millisecs
-                        domain : 'localhost',
                         sameSite: 'Lax',
                         httpOnly : true,
                         secure : true
                     })
                     res.cookie('r_token', arrayToken[1].encoded, {
                         maxAge : 31536000000, //millisecs
-                        domain : 'localhost',
                         sameSite: 'Lax',
                         httpOnly : true,
                         secure : true
