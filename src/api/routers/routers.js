741402bcc16028ad64fa01ada903c3ab117f0281
Lê Châu Trần Phát <lechautrphat@gmail.com>
2023-01-23 21:27:43 +0700

    final 22

--- src/api/routers/routers.js
+++ src/api/routers/routers.js
@@ -20,7 +20,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 };
 
